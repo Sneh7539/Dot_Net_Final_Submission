4ff784d9c02abc10c01c1061468a877313a0860d
Sneh Patel <snehpatel690@gmail.com>
2020-05-29 19:06:28 +0530

    Infix To Postfix Conversion

---
+++ Practical 5/Practical 5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The String In Infix Format : ");
+            string infix = Console.ReadLine();
+            string postfix;
+            InfixToPostfix infixToPostfix = new InfixToPostfix();
+            infixToPostfix.Converter(ref infix, out postfix);
+            Console.WriteLine("String In Postfix Format : ");
+            Console.WriteLine(postfix);
+            Console.Read();
+        }
+    }
+}
