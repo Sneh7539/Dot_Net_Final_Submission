9f6e7085570afc0fe0dac31e31b52cb6ae6fdd6b
Sneh Patel <snehpatel690@gmail.com>
2020-05-29 19:10:22 +0530

    Temperature Conversion From CS To F And F To CS

---
+++ Practical 8/Practical 8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Choice");
+            Console.WriteLine("1. Celcius to Fenrenheit\n2. Fehrenheit to Celcius");
+            int choice = int.Parse(Console.ReadLine());
+
+            float celcius, fahrenheit;
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter amouunt of temperature in Celcius");
+                    celcius = float.Parse(Console.ReadLine());
+                    fahrenheit = celcius * 9 / 5 + 32;
+                    Console.WriteLine("Celcius : " + celcius + "\nFahrenheit : " + fahrenheit);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter amouunt of temperature in Fahrenheit");
+                    fahrenheit = float.Parse(Console.ReadLine());
+                    celcius = (fahrenheit - 32) * 5 / 9;
+                    Console.WriteLine("Fahrenheit : " + fahrenheit + "\nCelcius : " + celcius);
+                    break;
+
+
+                default:
+                    Console.WriteLine("Sorry! Wrong Choice.");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
