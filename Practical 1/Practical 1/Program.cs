50de72143c96484fad6d95d6cd271dd6765637ec
Sneh Patel <snehpatel690@gmail.com>
2020-05-29 19:02:33 +0530

    Printing Sqaure Pattern

---
+++ Practical 1/Practical 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_1
+{
+    class Program
+    {
+        static void Pattern1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Pattern1();
+            Console.ReadLine();
+        }
+    }
+}
