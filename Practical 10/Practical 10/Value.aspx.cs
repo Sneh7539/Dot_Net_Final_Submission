3d3b605a4f560b8d8d1ffac6f3de1509885891e2
Sneh7539 <snehpatel690@gmail.com>
2020-05-29 19:36:34 +0530

    Program To Check Whether Empty Query String Is Entered

---
+++ Practical 10/Practical 10/Value.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical_10
+{
+    public partial class Value : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                Response.Redirect("Output.aspx?Usrnm = " + Usrnm.Text + "&Pwd = " + Pwd.Text + "");
+            }
+        }
+    }
+}
